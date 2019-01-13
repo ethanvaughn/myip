3cf170f7eb95756036fc1d9f5f005254c3d803ec
ethanvaughn <ethan.vaughn@gmail.com>
2019-01-13 05:42:44 +0000

    Added commit and push

--- myip.sh
+++ myip.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 myip=$(curl -s https://whatsmyip.com/ | grep "Your Public IPv4" | cut -d '>' -f 2 | cut -d '<' -f 1)
+echo $myip
 sed -i "s/^Current IP Addr:.*$/Current IP Addr: $myip/" README.md
+git commit -m "." README.md
+git push
 
 exit 0
