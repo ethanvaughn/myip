5749255ec3bb559911e8fde807cc23b8ef7a0f01
ethanvaughn <ethan.vaughn@gmail.com>
2019-08-31 17:17:45 -0600

    Updates script to use xmission.

--- myip.sh
+++ myip.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-myip=$(curl -s https://whatsmyip.com/ | grep "Your Public IPv4" | cut -d '>' -f 2 | cut -d '<' -f 1)
+curl -s http://ipv4.xmission.com > /tmp/ipv4.html
+myip=$(cat /tmp/ipv4.html | grep 'span.*class=.data.*>24' | tr -s ' ' | cut -d '>' -f2 | cut -d '<' -f1)
 echo $myip
 sed -i "s/^Current IP Addr:.*$/Current IP Addr: $myip/" README.md
 git commit -m "." README.md
