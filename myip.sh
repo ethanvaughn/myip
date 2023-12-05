6ba7c63babc78e28c0d5e891395ebadec12f8e28
Ethan Vaughn <ethan.vaughn@gmail.com>
2023-12-04 17:12:12 -0700

    Update the grep line.

--- myip.sh
+++ myip.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 curl -s http://ipv4.xmission.com > /tmp/ipv4.html
-myip=$(cat /tmp/ipv4.html | grep 'span.*class=.data.*>24' | tr -s ' ' | cut -d '>' -f2 | cut -d '<' -f1)
+myip=$(cat /tmp/ipv4.html | grep 'Your Actual IP Address:' | tr -s ' ' | cut -d '>' -f4 | cut -d '<' -f1)
 echo $myip
 sed -i "s/^Current IP Addr:.*$/Current IP Addr: $myip/" README.md
 git commit -m "." README.md
